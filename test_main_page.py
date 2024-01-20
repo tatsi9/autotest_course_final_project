6525635315dede608e167908477447e0720e3f8d
Ta <tatsi9it@yandex.ru>
2024-01-20 09:45:26 +0300

    modified files pages/main_page.py, test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)       # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                          # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+
+
+
+
+
+# версия из шага 4.1.8 с отдельной функцией def go_to_login_page
+
+#def go_to_login_page(browser):
+#    login_link = browser.find_element_by_css_selector("#login_link")
+#    login_link.click()
+
+#def test_guest_can_go_to_login_page(browser):
+#    link = "http://selenium1py.pythonanywhere.com/" 
+#    browser.get(link)
+#    go_to_login_page(browser)
+
+
+# первоначальная версия из шага 4.1.6
+#def test_guest_can_go_to_login_page(browser):
+#    link = "http://selenium1py.pythonanywhere.com/"
+#    browser.get(link)
+#    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+#    login_link.click()
