571f082e9dca277c16377862547142ad704ee66d
Ta <tatsi9it@yandex.ru>
2024-01-20 17:18:47 +0300

    added locators.py, login_page.py, scheme.png; modified main_page.py, test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)       # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                          # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()              # доб.в шаге 4.2.9(2) 
+    login_page = LoginPage(browser, browser.current_url) # доб.в шаге 4.2.9(2)
+    #login_page = page.go_to_login_page()          #переходим на страницу логина изм. в шаге 4.2.9
+    login_page.should_be_login_page()             #изм. в шаге 4.2.9
+
 
 
 def test_guest_should_see_login_link(browser):
