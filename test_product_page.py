baf52acdd91554682d2113e67e37eca9b191481e
Ta <tatsi9it@yandex.ru>
2024-01-24 17:57:28 +0300

    add need_review marks for 4 tests in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -34,7 +34,8 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_added_to_basket_message()
 
-
+    
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser, link)
@@ -53,6 +54,7 @@ class TestUserAddToBasketFromProductPage():
 @pytest.mark.parametrize('promo_offer', ["?promo=newYear"])
 
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo_offer}"
 
@@ -80,6 +82,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -114,6 +117,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_disappear_added_to_basket_message()
     
 # тест из п.4.3.10 - переход на страницу корзины со страницы продукта
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
